fc3d108382041cc700c64aa1366ac20cf997b135
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-07-20 13:27:52 +1000

    Add files via upload

---
+++ bandicamplus.c
@@ -0,0 +1,243 @@
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam fixer performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam fixer video quality
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam fixer video definition
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+
+//Bandicam fixer video recording
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam fixer video DV
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam video restoration
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam video restorated
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam video restoration quality and definition
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam video restoration resolution
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 0;
+int sum = 1;
+
+//Bandicam video restoration DPI and DV
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam fixer video file size
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam video restoration size
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam video restoration H.264
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam restoration recording video
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam restoration performance for video recording
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam optimizing application and video recording
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam restoration interface and interface design
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Bandicam restoration UI and UI design
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+
+//Compiler
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Interpreter
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Hybrid
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
